c9f618ddd8c0bf8c2519c6e13b45e251d21b7f33
Aqsa Aqeel <aqsa.aqeel17@example.com>
2023-02-10 03:07:05 +0530

    refactor : removed redundant icon

--- pages/index.js
+++ pages/index.js
@@ -37,12 +37,12 @@ export default function Home() {
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">Aqsa Aqeel</h1>
             <ul className="flex items-center">
-              <li>
+              {/* <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode((darkMode) => !darkMode)}
                   className="cursor-pointer text-xl"
                 />
-              </li>
+              </li> */}
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
