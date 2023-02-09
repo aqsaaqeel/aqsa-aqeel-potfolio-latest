8669dc22feca7929e694a2795bf26c730c5cfdb4
Aqsa Aqeel <aqsa.aqeel17@example.com>
2023-02-10 03:03:44 +0530

    fix : added resume link

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="https://drive.google.com/file/d/1p61CuG0UDFiDiZG_dapsykh4gDbL_j6G/view?usp=sharing"
                 >
                   Resume
                 </a>
